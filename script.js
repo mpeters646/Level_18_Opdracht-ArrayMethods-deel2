cd6af3024ec24d41bb902758e8cc2d921ec6725a
Marco Peters <m.peters646@gmail.com>
2021-03-10 08:18:37 +0100

    Updated C

--- script.js
+++ script.js
@@ -49,15 +49,11 @@ isItalyInTheGreat7(['Canada', 'France', 'Germany', 'Italy', 'Japan', 'United Kin
 
 // Opdracht E
 // Gebruik .forEach
-const tenfold = (arr) => {
-  const res = arr.forEach((number) => {
-    console.log(number);
-    return number * 10;
-  });
-  console.log(res);
+const tenfold = (numbers) => {
+  console.log(numbers);
+  return numbers.forEach((number) => number * 10);
 };
 
-console.log(tenfold(1));
 console.log(tenfold([1, 4, 3, 6, 9, 7, 11]));
 // result should be [10, 40, 30, 60, 90, 70, 110]
 
